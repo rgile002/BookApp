65050b24ad589f88de844f97974b6c373576448e
Rolf Gilet <rolf.gilet@ggainc.com>
2019-07-19 12:49:00 -0400

    Add DTO classes, Route Attributes, and controller actions( Get Book Details, By genre, By Author id, by publication date)

---
+++ BooksApi/DTOs/BookDto.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace BooksApi.DTOs
+{
+    public class BookDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
